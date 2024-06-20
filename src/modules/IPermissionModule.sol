813d66d048a3cb2f18625650223e2f43c9baaec4
Conner Swenberg <conner.swenberg@coinbase.com>
2024-06-20 10:52:58 -0400

    Update natspec for module interface

--- src/modules/IPermissionModule.sol
+++ src/modules/IPermissionModule.sol
@@ -1,9 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-/// @title IScopeVerifier
+/// @title IPermissionModule
 ///
 /// @author Coinbase (https://github.com/coinbase/smart-wallet)
 interface IPermissionModule {
+    /// @notice Validate the permissions for a session.
+    ///
+    /// @param account, the EIP-4337 account validating for.
+    /// @param sessionId, hash of the Session struct this permission check is validating.
+    /// @param permissionData, dynamic data stored in the session for validation.
+    /// @param requestData, dynamic data about the request to prove validation.
+    ///
+    /// @dev Reverts if validation does not pass; worth considering returning a magic-value.
     function validatePermissions(address account, bytes32 hash, bytes32 sessionId, bytes calldata permissionData, bytes calldata requestData) external view;
 }

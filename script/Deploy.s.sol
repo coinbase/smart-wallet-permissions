cb985e01139c9c093ab4a176bb1682bab2063493
Conner Swenberg <conner.swenberg@coinbase.com>
2024-07-18 13:45:48 -0700

    Add Click sample contract

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -6,16 +6,18 @@ import {Strings} from "openzeppelin-contracts/contracts/utils/Strings.sol";
 
 import {PermissionManager} from "../src/PermissionManager.sol";
 import {CallWithPermission} from "../src/permissions/CallWithPermission/CallWithPermission.sol";
+import {Click} from "../src/permissions/CallWithPermission/examples/Click.sol";
 
-// forge script script/Deploy.s.sol:Deploy --sender $SENDER --keystore $KS --password $PW --rpc-url $BASE_SEPOLIA_RPC --verify --verifier-url $SEPOLIA_BASESCAN_API --etherscan-api-key $BASESCAN_API_KEY --broadcast -vvvv
+// forge script Deploy --account dev --rpc-url $BASE_SEPOLIA_RPC --verify --verifier-url $SEPOLIA_BASESCAN_API --etherscan-api-key $BASESCAN_API_KEY --broadcast -vvvv
 contract Deploy is Script {
     function run() public {
         vm.startBroadcast();
 
-        PermissionManager sessionManager = new PermissionManager();
-        logAddress("PermissionManager", address(sessionManager));
+        // PermissionManager sessionManager = new PermissionManager();
+        // logAddress("PermissionManager", address(sessionManager));
         // CallWithPermission sessionCallPermission = new CallWithPermission();
         // logAddress("CallWithPermission", address(sessionCallPermission));
+        Click click = new Click();
 
         vm.stopBroadcast();
     }

e2543594442aae75b1d5c17531defe24fb816e1e
Conner Swenberg <conner.swenberg@coinbase.com>
2024-08-06 18:52:14 -0700

    Fix contract versions to support latest

--- src/PermissionManager.sol
+++ src/PermissionManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.23;
+pragma solidity ^0.8.23;
 
 import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 import {IERC1271} from "openzeppelin-contracts/contracts/interfaces/IERC1271.sol";
--- src/permissions/IPermissionContract.sol
+++ src/permissions/IPermissionContract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.23;
+pragma solidity ^0.8.23;
 
 import {UserOperation} from "account-abstraction/interfaces/UserOperation.sol";
 

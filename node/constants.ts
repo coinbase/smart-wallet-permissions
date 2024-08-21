d17e4004cacf326a41b4d71aaed9846954cc9e27
Conner Swenberg <conner.swenberg@coinbase.com>
2024-08-21 11:31:56 -0700

    Add sample request to docs

--- node/constants.ts
+++ node/constants.ts
@@ -1,6 +1,7 @@
-import { Address } from "viem";
+import { Address, Hex } from "viem";
 
 export const PermissionManager = 
   "0x98549c7A6513c20b66FacAd469b3d94a3B7B6992" as Address;
 export const PermissionCallableAllowedContractNativeTokenRecurringAllowance =
   "0x08f955BC6665250c8Ad546Ba225b2352bC6Cd1d5" as Address;
+export const PermissionedCallSelector = "0x2bd1b86d" as Hex

53e02c12caf98d81979a42827d817da946aa7e7f
Conner Swenberg <conner.swenberg@coinbase.com>
2024-08-23 22:37:14 -0700

    Add utils test coverage skeletons

---
+++ test/utils/BytesLib/Eq.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {Test, console2} from "forge-std/Test.sol";
+
+contract EqTest is Test {
+    function setUp() public {}
+
+    function test_eq_true_sameInputs() public {}
+
+    function test_eq_false_differentInputs() public {}
+}

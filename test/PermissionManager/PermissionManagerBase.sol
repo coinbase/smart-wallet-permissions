a07399ed6b27b22ff355decbc80d80b1fc48910a
Conner Swenberg <conner.swenberg@coinbase.com>
2024-08-27 08:45:25 -0400

    Add Pause test

--- test/PermissionManager/PermissionManagerBase.sol
+++ test/PermissionManager/PermissionManagerBase.sol
@@ -1,4 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-contract PermissionManagerBase {}
+import {Test, console2} from "forge-std/Test.sol";
+
+import {PermissionManager} from "../../src/PermissionManager.sol";
+
+contract PermissionManagerBase is Test {
+    PermissionManager permissionManager;
+    address owner = address(0xbabe);
+    uint256 cosignerPrivateKey = 0xa11ce;
+    address cosigner = vm.addr(cosignerPrivateKey);
+
+    function _initializePermissionManager() internal {
+        permissionManager = new PermissionManager(owner, cosigner);
+    }
+}

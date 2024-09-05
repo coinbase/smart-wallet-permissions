68d099b176175747d75843ddf9e2a90b38d6e4f9
Conner Swenberg <conner.swenberg@coinbase.com>
2024-09-04 21:25:02 -0700

    Add BytesLib tests

--- test/utils/BytesLib/Eq.t.sol
+++ test/utils/BytesLib/Eq.t.sol
@@ -3,14 +3,17 @@ pragma solidity ^0.8.23;
 
 import {Test, console2} from "forge-std/Test.sol";
 
+import {BytesLib} from "../../../src/utils/BytesLib.sol";
+
 contract EqTest is Test {
     function setUp() public {}
 
-    function test_eq_true_sameInputs() public {
-        revert("unimplemented");
+    function test_eq_true_sameInputs(bytes memory data) public {
+        assertTrue(BytesLib.eq(data, data));
     }
 
-    function test_eq_false_differentInputs() public {
-        revert("unimplemented");
+    function test_eq_false_differentInputs(bytes memory a, bytes memory b) public {
+        vm.assume(keccak256(a) != keccak256(b));
+        assertTrue(!BytesLib.eq(a, b));
     }
 }
--- test/utils/BytesLib/TrimSelector.t.sol
+++ test/utils/BytesLib/TrimSelector.t.sol
@@ -3,10 +3,19 @@ pragma solidity ^0.8.23;
 
 import {Test, console2} from "forge-std/Test.sol";
 
+import {BytesLib} from "../../../src/utils/BytesLib.sol";
+
 contract TrimSelectorTest is Test {
     function setUp() public {}
 
-    function test_trimSelector_eq_calldataSlice() public {
-        revert("unimplemented");
+    function test_trimSelector_eq_args(bytes4 selector, bytes memory data) public {
+        bytes memory trimmed = BytesLib.trimSelector(abi.encodeWithSelector(selector, data));
+        assertEq(trimmed, abi.encode(data));
+    }
+
+    function test_trimSelector_eq_calldataSlice(bytes calldata data) public {
+        vm.assume(data.length > 4);
+        bytes memory trimmed = BytesLib.trimSelector(data);
+        assertEq(trimmed, data[4:]);
     }
 }

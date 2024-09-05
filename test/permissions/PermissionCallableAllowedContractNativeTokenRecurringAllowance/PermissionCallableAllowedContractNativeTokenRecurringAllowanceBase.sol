e2e93be02177471b4e9aebaf754dec8e14b169fb
Conner Swenberg <conner.swenberg@coinbase.com>
2024-09-04 20:46:14 -0700

    Add validatePermission tests

--- test/permissions/PermissionCallableAllowedContractNativeTokenRecurringAllowance/PermissionCallableAllowedContractNativeTokenRecurringAllowanceBase.sol
+++ test/permissions/PermissionCallableAllowedContractNativeTokenRecurringAllowance/PermissionCallableAllowedContractNativeTokenRecurringAllowanceBase.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.8.23;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {MagicSpend} from "magic-spend/MagicSpend.sol";
+import {CoinbaseSmartWallet} from "smart-wallet/CoinbaseSmartWallet.sol";
 
+import {IPermissionCallable} from "../../../src/interfaces/IPermissionCallable.sol";
+import {NativeTokenRecurringAllowance} from "../../../src/mixins/NativeTokenRecurringAllowance.sol";
 import {PermissionCallableAllowedContractNativeTokenRecurringAllowance as PermissionContract} from
     "../../../src/permissions/PermissionCallableAllowedContractNativeTokenRecurringAllowance.sol";
 
@@ -33,6 +36,52 @@ contract PermissionCallableAllowedContractNativeTokenRecurringAllowanceBase is
         return PermissionContract.PermissionValues({
             recurringAllowance: _createRecurringAllowance(start, period, allowance),
             allowedContract: allowedContract
+        });
+    }
+
+    function _createPermissionValues(uint160 allowance, address allowedContract)
+        internal
+        returns (PermissionContract.PermissionValues memory)
+    {
+        return PermissionContract.PermissionValues({
+            recurringAllowance: _createRecurringAllowance({start: 1, period: type(uint24).max, allowance: allowance}),
+            allowedContract: allowedContract
+        });
+    }
+
+    function _createPermissionedCall(address target, uint256 value, bytes memory data)
+        internal
+        returns (CoinbaseSmartWallet.Call memory)
+    {
+        return CoinbaseSmartWallet.Call({
+            target: target,
+            value: value,
+            data: abi.encodeWithSelector(IPermissionCallable.permissionedCall.selector, data)
+        });
+    }
+
+    function _createUseRecurringAllowanceCall(address target, bytes32 permissionHash, uint256 spend)
+        internal
+        returns (CoinbaseSmartWallet.Call memory)
+    {
+        return CoinbaseSmartWallet.Call({
+            target: target,
+            value: 0,
+            data: abi.encodeWithSelector(PermissionContract.useRecurringAllowance.selector, permissionHash, spend)
+        });
+    }
+
+    function _createWithdrawCall(address target, address asset, uint256 amount)
+        internal
+        returns (CoinbaseSmartWallet.Call memory)
+    {
+        return CoinbaseSmartWallet.Call({
+            target: target,
+            value: 0,
+            data: abi.encodeWithSelector(
+                MagicSpend.withdraw.selector,
+                MagicSpend.WithdrawRequest({signature: hex"", asset: asset, amount: amount, nonce: 0, expiry: 0})
+            )
         });
     }
 }

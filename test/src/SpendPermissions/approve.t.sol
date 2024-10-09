b49a739d3bf780795607bcdade2791f36d42f3f1
Amie Corso <amie.corso@coinbase.com>
2024-10-09 15:45:00 -0700

    tests for approve

--- test/src/SpendPermissions/approve.t.sol
+++ test/src/SpendPermissions/approve.t.sol
@@ -12,6 +12,79 @@ contract ApproveTest is Test, SpendPermissionsBase {
         _initializeSpendPermissions();
     }
 
-    function test_approve_revert_invalidSender() public {}
-    function test_approve_success() public {}
+    function test_approve_revert_invalidSender(
+        address sender,
+        address account,
+        address permissionSigner,
+        uint48 start,
+        uint48 end,
+        uint48 period,
+        uint160 allowance
+    ) public {
+        vm.assume(sender != address(0));
+        vm.assume(sender != account);
+
+        SpendPermissions.RecurringAllowance memory recurringAllowance = SpendPermissions.RecurringAllowance({
+            account: account,
+            spender: permissionSigner,
+            token: ETHER,
+            start: start,
+            end: end,
+            period: period,
+            allowance: allowance
+        });
+        vm.startPrank(sender);
+        vm.expectRevert(abi.encodeWithSelector(SpendPermissions.InvalidSender.selector, account));
+        mockSpendPermissions.approve(recurringAllowance);
+        vm.stopPrank();
+    }
+
+    function test_approve_success_isAuthorized(
+        address account,
+        address permissionSigner,
+        uint48 start,
+        uint48 end,
+        uint48 period,
+        uint160 allowance
+    ) public {
+        SpendPermissions.RecurringAllowance memory recurringAllowance = SpendPermissions.RecurringAllowance({
+            account: account,
+            spender: permissionSigner,
+            token: ETHER,
+            start: start,
+            end: end,
+            period: period,
+            allowance: allowance
+        });
+        vm.prank(account);
+        mockSpendPermissions.approve(recurringAllowance);
+        vm.assertTrue(mockSpendPermissions.isAuthorized(recurringAllowance));
+    }
+
+    function test_approve_success_emitsEvent(
+        address account,
+        address permissionSigner,
+        uint48 start,
+        uint48 end,
+        uint48 period,
+        uint160 allowance
+    ) public {
+        SpendPermissions.RecurringAllowance memory recurringAllowance = SpendPermissions.RecurringAllowance({
+            account: account,
+            spender: permissionSigner,
+            token: ETHER,
+            start: start,
+            end: end,
+            period: period,
+            allowance: allowance
+        });
+        vm.startPrank(account);
+        vm.expectEmit(address(mockSpendPermissions));
+        emit SpendPermissions.RecurringAllowanceApproved({
+            hash: mockSpendPermissions.getHash(recurringAllowance),
+            account: account,
+            recurringAllowance: recurringAllowance
+        });
+        mockSpendPermissions.approve(recurringAllowance);
+    }
 }

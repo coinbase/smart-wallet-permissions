97572471868b3f6329d589915b5304cb7736189f
Amie Corso <amie.corso@coinbase.com>
2024-10-09 15:56:30 -0700

    tests for getHash

--- test/src/SpendPermissions/getHash.t.sol
+++ test/src/SpendPermissions/getHash.t.sol
@@ -6,11 +6,86 @@ import {Test, console2} from "forge-std/Test.sol";
 import {SpendPermissions} from "../../../src/SpendPermissions.sol";
 
 import {SpendPermissionsBase} from "../../base/SpendPermissionsBase.sol";
+import {MockSpendPermissions} from "../../mocks/MockSpendPermissions.sol";
 
 contract GetHashTest is Test, SpendPermissionsBase {
     function setUp() public {
         _initializeSpendPermissions();
     }
 
-    function test_getHash_success() public {}
+    function test_getHash_success(
+        address account,
+        address spender,
+        address token,
+        uint48 start,
+        uint48 end,
+        uint48 period,
+        uint160 allowance
+    ) public {
+        SpendPermissions.RecurringAllowance memory recurringAllowance = SpendPermissions.RecurringAllowance({
+            account: account,
+            spender: spender,
+            token: token,
+            start: start,
+            end: end,
+            period: period,
+            allowance: allowance
+        });
+        bytes32 hash = mockSpendPermissions.getHash(recurringAllowance);
+    }
+
+    function test_getHash_success_uniqueHashPerChain(
+        address account,
+        address spender,
+        address token,
+        uint48 start,
+        uint48 end,
+        uint48 period,
+        uint160 allowance,
+        uint64 chainId1,
+        uint64 chainId2
+    ) public {
+        vm.assume(chainId1 != chainId2);
+        vm.assume(chainId1 > 0);
+        vm.assume(chainId2 > 0);
+        SpendPermissions.RecurringAllowance memory recurringAllowance = SpendPermissions.RecurringAllowance({
+            account: account,
+            spender: spender,
+            token: token,
+            start: start,
+            end: end,
+            period: period,
+            allowance: allowance
+        });
+        vm.chainId(chainId1);
+        bytes32 hash1 = mockSpendPermissions.getHash(recurringAllowance);
+        vm.chainId(chainId2);
+        bytes32 hash2 = mockSpendPermissions.getHash(recurringAllowance);
+        assertNotEq(hash1, hash2);
+    }
+
+    function test_getHash_success_uniqueHashPerContract(
+        address account,
+        address spender,
+        address token,
+        uint48 start,
+        uint48 end,
+        uint48 period,
+        uint160 allowance
+    ) public {
+        SpendPermissions.RecurringAllowance memory recurringAllowance = SpendPermissions.RecurringAllowance({
+            account: account,
+            spender: spender,
+            token: token,
+            start: start,
+            end: end,
+            period: period,
+            allowance: allowance
+        });
+        MockSpendPermissions mockSpendPermissions1 = new MockSpendPermissions();
+        MockSpendPermissions mockSpendPermissions2 = new MockSpendPermissions();
+        bytes32 hash1 = mockSpendPermissions1.getHash(recurringAllowance);
+        bytes32 hash2 = mockSpendPermissions2.getHash(recurringAllowance);
+        assertNotEq(hash1, hash2);
+    }
 }
